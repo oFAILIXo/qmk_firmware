a9eea49548a47cd8e18ebf3f07271f0006d42567
oFAILIXo <felgrave@gmail.com>
2017-02-17 15:56:51 +0100

    Update keymap.c
    Added TD_COLN_SCLN line 56-63 and layer0 semicolon key

--- keyboards/gonnerd/keymaps/oFAILIXo/keymap.c
+++ keyboards/gonnerd/keymaps/oFAILIXo/keymap.c
@@ -12,11 +12,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 [BASE_LAYER] = KEYMAP_60(
-    KC_ESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    KC_EQL,    KC_BSLS,    KC_GRV,\
-    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC,               KC_DEL,\
-    OSL(1),        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    __x__,                 KC_ENT,\
-    OSM(MOD_LSFT), __x__,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    MT(MOD_LSFT,KC_RALT),  TO(2), \
-    OSM(MOD_LCTL), KC_LGUI, OSM(MOD_LALT),                      LT(4, KC_SPC),                      KC_RALT, KC_APP,     MO(3),                 KC_RCTL),
+    KC_ESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,              KC_MINS,    KC_EQL,    KC_BSLS,    KC_GRV,\
+    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,              KC_LBRC,    KC_RBRC,               KC_DEL,\
+    OSL(1),        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_COLN_SCLN),  KC_QUOT,    __x__,                 KC_ENT,\
+    OSM(MOD_LSFT), __x__,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH,    MT(MOD_LSFT,KC_RALT),  TO(2), \
+    OSM(MOD_LCTL), KC_LGUI, OSM(MOD_LALT),                      LT(4, KC_SPC),                      KC_RALT,           KC_APP,     MO(3),                 KC_RCTL),
 
 	
 [FUNCTION_LAYER] = KEYMAP_60(
@@ -49,5 +49,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS,KC_BSPC,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_TRNS,KC_MS_L,KC_MS_D,KC_MS_R,KC_DEL, KC_TRNS,__x__,  KC_TRNS,\
     KC_TRNS,__x__,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,\
     KC_TRNS,KC_TRNS,KC_TRNS,                        KC_SPC,                         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS),
+};
+
+//Tap Dance Declarations
+enum {
+  TD_COLN_SCLN = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Colon, twice for Semicolon
+  [TD_COLN_SCLN]  = ACTION_TAP_DANCE_DOUBLE(KC_COLN, KC_SCLN)
+// Other declarations would go here, separated by commas, if you have them
 };
 
